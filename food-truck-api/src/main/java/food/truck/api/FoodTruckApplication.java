7d91420670e880deb9bfa47b7b52e86929c68634
John Lutteringer <john.lutteringer@gmail.com>
2020-09-08 10:21:48 -0500

    initial commit from source

---
+++ food-truck-api/src/main/java/food/truck/api/FoodTruckApplication.java
@@ -0,0 +1,11 @@
+package food.truck.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTruckApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodTruckApplication.class);
+    }
+}
