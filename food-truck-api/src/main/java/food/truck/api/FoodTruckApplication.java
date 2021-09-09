cdbb8457460b3ec78c675c46081dbc5e7a63ae5f
Rahaman <mdshahidur_rahaman1@baylor.edu>
2021-09-09 17:36:49 -0500

    create separate branch and update the existence project

--- food-truck-api/src/main/java/food/truck/api/FoodTruckApplication.java
+++ food-truck-api/src/main/java/food/truck/api/FoodTruckApplication.java
@@ -2,10 +2,11 @@ package food.truck.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class FoodTruckApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FoodTruckApplication.class);
+        SpringApplication.run(FoodTruckApplication.class, args);
     }
 }
