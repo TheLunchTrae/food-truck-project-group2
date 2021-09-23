c89cbb26b0de50dd8941a7ba004a42274652fb3e
Fireman89 <sam_hobbs1@baylor.edu>
2021-09-23 17:37:16 -0500

    commented out import react

--- food-truck-frontend/src/pages/index.js
+++ food-truck-frontend/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 import Link from '@material-ui/core/Link';
 require('dotenv').config();
 import {BrowserRouter, Switch, Route, Router} from "react-router-dom";
