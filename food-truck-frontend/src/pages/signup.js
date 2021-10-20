04ba603313aa76b28be036133bd4746a304bfd4e
Tyler Ross <Tyler_Ross3@Baylor.edu>
2021-10-20 15:47:14 -0500

    Front end sign up form

--- food-truck-frontend/src/pages/signup.js
+++ food-truck-frontend/src/pages/signup.js
@@ -3,19 +3,42 @@ import React, { Component } from 'react';
 class Signup extends Component {
     constructor(props) {
         super();
-    }
-    handleChangeStatus(event) {
+        this.state = {email: '', password: ''};
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleInputChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+        
+        this.setState({
+            [name]: value
+        });
     }
     handleSubmit(event) {
-    }
-    componentDidMount() {
+        alert("Email: " + this.state.email + "\nPassword: " + this.state.password);
     }
     render() {
         return (
             <div>
-                This is the signup page! :)
+                <h1>Sign Up</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label>
+                            Email:
+                            <input name="email" value={this.state.email} type="text" onChange={this.handleInputChange}/>
+                        </label>
+                    </div>
+                    <br/>
+                    <div>
+                        <label>
+                            Password:
+                            <input name="password" value={this.state.password} type="text" onChange={this.handleInputChange}/>
+                        </label>
+                    </div>
+                    <input type="submit" value="Submit"/>
+                </form>
+                <br/>
             </div>
         );
     }
