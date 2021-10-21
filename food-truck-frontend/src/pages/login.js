3e769c870a0741cd800a678540057362cf920745
Tyler Ross <Tyler_Ross3@Baylor.edu>
2021-10-20 21:28:03 -0500

    login page

--- food-truck-frontend/src/pages/login.js
+++ food-truck-frontend/src/pages/login.js
@@ -3,19 +3,59 @@ import React, { Component } from 'react';
 class Login extends Component {
     constructor(props) {
         super();
-    }
-    handleChangeStatus(event) {
+        this.state = {email: '', password: ''};
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleInputChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+        
+        this.setState({
+            [name]: value
+        });
     }
     handleSubmit(event) {
-    }
-    componentDidMount() {
+        alert("Email: " + this.state.email + "\nPassword: " + this.state.password);
+        const userDto = {
+            emailAddress: this.state.email,
+            password: this.state.password,
+            userType: "Customer"
+        };
+
+        const instance = axios.create({baseURL: process.env.FOOD_TRUCK_API_URL})
+
+        instance.post('/login', userDto).then(res => {
+            console.log("User login")
+            this.setState({
+                email: userDto.emailAddress,
+                password: userDto.password
+            });
+            alert("Email state: " + this.email + "\nPassword state: " + this.password);
+        })
+
+        event.preventDefault()
     }
     render() {
         return (
-            <div>
-                This is the login page! :)
+            <div align="middle">
+                <h1>Log In</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label>
+                            Email:
+                            <input name="email" value={this.state.email} type="text" onChange={this.handleInputChange}/>
+                        </label>
+                    </div>
+                    <br/>
+                    <div>
+                        <label>
+                            Password:
+                            <input name="password" value={this.state.password} type="text" onChange={this.handleInputChange}/>
+                        </label>
+                    </div>
+                    <input type="submit" value="Submit"/>
+                </form>
             </div>
         );
     }
