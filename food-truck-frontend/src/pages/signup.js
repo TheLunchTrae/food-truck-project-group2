1af0d9634c9eded7869888d3511c98f491e62ef9
JoshuaHunter16 <jchunter816@gmail.com>
2021-11-04 11:23:45 -0500

    HTML reconfig to the signup page...this is also going to need to be fixed for pages signup mirrored

--- food-truck-frontend/src/pages/signup.js
+++ food-truck-frontend/src/pages/signup.js
@@ -46,37 +46,53 @@ class Signup extends Component {
     render() {
         return (
             <html>
-                <body style = {{backgroundColor: '#FFDAB9', marginLeft: '35px'}}>
+                <body style = {{backgroundColor: '#FFDAB9'}}>
                     <div>
                         <div class="sections" >
-                            <div class = "signup" style = {{alignContent: 'center', float: 'left', borderRadius: '100px', background: '#FA8072', width: '25%', padding: '20px', display: 'inline-block', marginTop: '20px', marginLeft: '670px'}}>
+                            <div class = "signup" style = {{alignContent: 'center', borderRadius: '100px', background: '#FA8072', width: '30%', padding: '20px', margin: '35px auto', textAllign: 'center'}}>
                                 <form onSubmit={this.handleSubmit}>
-                                    <span class="heading" style={{color: "#0F52BA", display: 'block', fontSize: '4.5rem', textAlign: 'center', fontWeight: 'bold'}}>Sign Up</span>
-                                    <span class = "username" style = {{marginLeft: '27px', display: 'inline-block', fontSize: '1.5rem', textAlign: 'left', fontWeight: 'bold', marginTop: '5px'}}>Username:</span>
-                                        <span id = "usernameInput" style={{fontSize: '1.5rem', textAlign: 'left', marginLeft: '10px'}}>
+
+                                    <span class="heading" style={{color: "#0F52BA", display: 'block', fontSize: '2.5rem', textAlign: 'center', fontWeight: 'bold'}}>Sign Up</span>
+
+                                    <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
+                                        <span class = "username" style = {{fontSize: '1.4rem', fontWeight: 'bold', marginTop: '5px'}}>Username:</span>
+                                        <span id = "usernameInput" style={{fontSize: '1.4rem', marginLeft: '10px'}}>
                                             <input name="username" placeholder="Enter a Username" value={this.state.username} type="text" onChange={this.handleInputChange}/>
                                         </span>
-                                        <span class = "email" style = {{marginLeft: '80px', display: 'inline-block', fontSize: '1.5rem', textAlign: 'left', fontWeight: 'bold', marginTop: '5px'}}>Email:</span>
-                                        <span id = "emailInput" style={{fontSize: '1.5rem', textAlign: 'left', marginLeft: '10px'}}>
+                                    </div>
+
+                                    <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
+                                        <span class = "email" style = {{fontSize: '1.4rem', fontWeight: 'bold', marginTop: '5px'}}>Email:</span>
+                                        <span id = "emailInput" style={{fontSize: '1.4rem', marginLeft: '10px'}}>
                                             <input name="email" pattern=".*@.*\..*" title="Must be in valid email format" placeholder="Enter an email" value={this.state.email} type="text" onChange={this.handleInputChange}/>
                                         </span>
-                                    <label>
-                                        <span class = "password" style = {{marginLeft: '32px', display: 'inline-block', fontSize: '1.5rem', textAlign: 'left', fontWeight: 'bold', marginTop: '5px'}}>Password:</span>
-                                    </label>
-                                    <span id = "passwordInput" style={{fontSize: '1.5rem', textAlign: 'left', marginLeft: '10px'}}>
-                                        <input type="password" name="password" placeholder="Enter a password" value={this.state.password} onChange={this.handleInputChange}/>
-                                    </span>
+                                    </div>
+
+                                    <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
+                                        <label>
+                                            <span class = "password" style = {{fontSize: '1.4rem',fontWeight: 'bold', marginTop: '5px'}}>Password:</span>
+                                        </label>
+                                        <span id = "passwordInput" style={{fontSize: '1.4rem', marginLeft: '10px'}}>
+                                            <input type="password" name="password" placeholder="Enter a password" value={this.state.password} onChange={this.handleInputChange}/>
+                                        </span>
+                                    </div>
+
+
+                                    <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
+                                        <label>
+                                            <span class = "userType" style = {{fontSize: '1.4rem', fontWeight: 'bold', marginTop: '5px'}}>User Type:</span>
+                                        </label>
+                                        <span id = "type" style={{fontSize: '1.4rem', marginLeft: '10px'}}>
+                                            <select id="usertype">
+                                                <option value="Customer">Customer</option>
+                                                <option value="Owner">Food Truck Owner</option>
+                                            </select>
+                                        </span>
+                                    </div>
+
                                     <br></br>
-                                    <label>
-                                        <span class = "userType" style = {{marginLeft: '31px', display: 'inline', fontSize: '1.5rem', textAlign: 'left', fontWeight: 'bold', marginTop: '5px'}}>User Type:</span>
-                                    </label>
-                                    <span id = "type" style={{fontSize: '1.5rem', textAlign: 'left', marginLeft: '10px'}}>
-                                        <select id="usertype">
-                                            <option value="Customer">Customer</option>
-                                            <option value="Owner">Food Truck Owner</option>
-                                        </select>
-                                    </span>
-                                    <span style={{marginLeft: '200px'}}>
+
+                                    <span style={{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
                                         <input type="submit" value="Sign Up!"/>
                                     </span>
                                 </form>
