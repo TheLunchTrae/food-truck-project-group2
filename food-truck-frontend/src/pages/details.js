0b59434392351fcbe944e8982a12f427372914de
Tyler Ross <Tyler_Ross3@Baylor.edu>
2021-11-03 20:45:19 -0500

    Id on the details page

--- food-truck-frontend/src/pages/details.js
+++ food-truck-frontend/src/pages/details.js
@@ -1,9 +1,11 @@
+import { HighlightSharp } from '@material-ui/icons';
 import React, { Component } from 'react';
 import './_app.js'
 
 class Dashboard extends Component {
     constructor(props) {
-        super();
+        super(props);
+        this.state = { id: '' };
     }
     handleChangeStatus(event) {
     }
@@ -12,17 +14,19 @@ class Dashboard extends Component {
     handleSubmit(event) {
     }
     componentDidMount() {
+        // get gets the id from the url
         const queryString = window.location.search;
         console.log(queryString);
         const urlParams = new URLSearchParams(queryString);
         const id = urlParams.get('id');
         console.log(id);
+        this.setState({ id: id });
     }
     render() {
         return (
             <div>
                 <h1>
-                    Test
+                    Hello, {this.state.id}!
                 </h1>
             </div>
         
