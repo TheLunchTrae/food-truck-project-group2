a074d529f928005e7b8a73ac240feb8fcdb1581d
JoshuaHunter16 <jchunter816@gmail.com>
2021-11-04 17:28:20 -0500

    Adding a homepage that actually looks like one...not complete

--- food-truck-frontend/src/pages/index.js
+++ food-truck-frontend/src/pages/index.js
@@ -2,23 +2,51 @@ require('dotenv').config();
 
 function HomePage() {
     return (
-        <div>
-            This is the homepage to Group 2's front end! :)
-            <br/>
-            <a href = "\signup">Sign Up</a>
-            <br/>
-            <a href = "\login">Log In</a>
-            <br/>
-            <a href = "\search">Search</a>
-            <br/>
-            <a href = "\dashboard">Dashboard (Food Truck Owner)</a>
-            <br/>
-            <a href = "\details">Dashboard (Customer)</a>
-            <br/>
-            <a href = "\about">About</a>
-            <br/>
-            <a href = "\createTruck">Create Truck</a>
+        <div style = {{backgroundColor: '#90AACB', padding: '0', fontFamily: 'poppins'}}>
+            <div class="banner-area" style = {{ margin: 'auto 30px', animationName: 'animate', animationDuration: '7s', animationIterationCount: 'infinite', animationTimingFunction: 'ease-in-out', height: '100vh'}}>
+
+              <div class="wrapper" style = {{width: '1170px', margin: '0 auto'}}>
+                <div class="navigation" style = {{height: '60px'}}>
+                  <div class="logo" style = {{width: '20%', float: 'left', padding: '30px 0 0',fontSize: '25px', fontWeight: '700', color: '#FFFFFF'}}>
+                    Food Truck Finder
+                  </div>
+                  <nav style = {{width: '75%', float: 'right', textAlign: 'right', padding: '30px 0 0'}}>
+                    <a href = "\signup" style = {{textDecoration: 'none', color: '#FFFFFF', padding: '15px 20px'}}>Sign Up</a>
+                    <a href = "\login" style = {{textDecoration: 'none', color: '#FFFFFF', padding: '15px 20px'}}>Log In</a>
+                    <a href = "\search" style = {{textDecoration: 'none', color: '#FFFFFF', padding: '15px 20px'}}>Search</a>
+                    <a href = "\dashboard" style = {{textDecoration: 'none', color: '#FFFFFF', padding: '15px 20px'}}>Dashboard (Food Truck Owner)</a>
+                    <a href = "\about" style = {{textDecoration: 'none', color: '#FFFFFF', padding: '15px 20px'}}>About</a>
+                  </nav>
+                </div>
+
+                <div class="banner-text">
+                  <div class="text-area" style = {{textAlign: 'center', width: '75%', margin: '0 auto'}}>
+                    <h2 style = {{fontSize: '75px', color: '#FFFFFF', margin: '10% 0 0'}}>Food Truck Finder</h2>
+                    <h3 style = {{color: '#FFFFFF', textTransform: 'uppercase', margin: '0 0 15px', fontSize: '35px'}}>Group 2</h3>
+                    <p style = {{fontSize: '18px', color: '#FFFFFF', width: '70%', margin: '0 auto', lineHeight: '1.9'}}>IDK WHAT YALL WANNA PUT HERE</p>
+                  </div>
+                </div>
+              </div>
+
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <div>REMOVE ONCE SESSIONS IMPLEMENTED, WE NEED TO PUT IN INDIVIDUAL PEOPLE</div>
+              <a href = "\details" style = {{textDecoration: 'none', color: '#FFFFFF', padding: '15px 20px'}}>Dashboard (Customer)</a>
+              <a href = "\editAccount" style = {{textDecoration: 'none', color: '#FFFFFF', padding: '15px 20px'}}>Edit Account</a>
+              <a href = "\editTruck" style = {{textDecoration: 'none', color: '#FFFFFF', padding: '15px 20px'}}>Edit Truck</a>
+              <a href = "\createTruck" style = {{textDecoration: 'none', color: '#FFFFFF', padding: '15px 20px'}}>Create Truck</a>
+            </div>
         </div>
+
+
     )
 }
 
