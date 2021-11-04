08d5a22a71b1a969c822946ad1b95d96a0551d84
Tyler Ross <Tyler_Ross3@Baylor.edu>
2021-11-03 22:05:07 -0500

    Changed id to name

--- food-truck-frontend/src/pages/details.js
+++ food-truck-frontend/src/pages/details.js
@@ -6,7 +6,7 @@ class Dashboard extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { id: '' };
+        this.state = { name: '' };
     }
     handleChangeStatus(event) {
     }
@@ -22,10 +22,10 @@ class Dashboard extends Component {
         const id = urlParams.get('id');
         console.log(id);
         if(id != null) {
-            this.setState({ id: id });
+            this.setState({ name: "Name here" });
         }
         else {
-            this.setState({id: "USER_NAME"});
+            this.setState({name: "USER_NAME"});
         }
 
 
@@ -44,7 +44,7 @@ class Dashboard extends Component {
                             <div class = "userSection" style = {{float: 'left', borderRadius: '100px', background: '#FA8072', width: '25%', padding: '20px', display: 'inline-block'}}>
                                 <div>
                                     <span style = {{height: '100px', width: '100px', background: '#bbbbbb', borderRadius: '50%', display: 'block', zIndex: '99', margin: '0 auto'}}></span>
-                                    <span class = "userName" style = {{color: '#0F52BA', display: 'block', fontSize: '1.5rem', textAlign: 'center', fontWeight: 'bold', marginTop: '5px'}}>{this.state.id}</span>
+                                    <span class = "userName" style = {{color: '#0F52BA', display: 'block', fontSize: '1.5rem', textAlign: 'center', fontWeight: 'bold', marginTop: '5px'}}>{this.state.name}</span>
                                     <span class = "userType" style = {{color: '#002366', display: 'block', fontSize: '1.1rem', textAlign: 'center'}}>Customer</span>  {/*THIS IS GOING TO BE THE TYPE OF USER THEY ARE LOGGED IN AS -->*/}
                                     <hr style = {{border: '1px solid black', width: '75%'}}></hr>
                                     <span class = "userType" style = {{color: '#0F52BA', display: 'block', fontSize: '1.1rem', textAlign: 'center', margin: '20px 0', fontWeight: 'bold'}}><u>Notifications</u></span>
