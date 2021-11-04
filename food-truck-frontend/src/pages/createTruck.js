27c758dd0f80f49f005e36baaba6447bff3f397d
JoshuaHunter16 <jchunter816@gmail.com>
2021-11-04 04:35:50 -0500

    Fixing all the create truck HTML stuff

--- food-truck-frontend/src/pages/createTruck.js
+++ food-truck-frontend/src/pages/createTruck.js
@@ -57,38 +57,58 @@ class Signup extends Component {
     render() {
         return (
             <html>
-                <body style = {{backgroundColor: '#FFDAB9', marginLeft: '35px'}}>
+                <body style = {{backgroundColor: '#FFDAB9'}}>
                     <div>
                         <div class="sections" >
-                            <div class = "signup" style = {{alignContent: 'center', float: 'left', borderRadius: '100px', background: '#FA8072', width: '50%', padding: '20px', display: 'inline-block', marginTop: '20px', marginLeft: '670px'}}>
+                            <div class = "createTruck" style = {{alignContent: 'center', borderRadius: '100px', background: '#FA8072', width: '40%', padding: '20px', margin: '35px auto', textAllign: 'center'}}>
                                 <form onSubmit={this.handleSubmit}>
-                                    <span class="heading" style={{color: "#0F52BA", display: 'block', fontSize: '4.5rem', textAlign: 'center', fontWeight: 'bold'}}>Create A New Truck</span>
-                                    <span class = "menu" style = {{marginLeft: '27px', display: 'inline-block', fontSize: '1.5rem', textAlign: 'left', fontWeight: 'bold', marginTop: '5px'}}>Menu:</span>
-                                        <span id = "menuInput" style={{fontSize: '1.5rem', textAlign: 'left', marginLeft: '10px'}}>
+                                    <span class="heading" style={{color: "#0F52BA", display: 'block', fontSize: '2.5rem', textAlign: 'center', fontWeight: 'bold'}}>Create A New Truck</span>
+
+                                    <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
+                                        <span class = "menu" style = {{fontSize: '1.4rem', fontWeight: 'bold', marginTop: '5px'}}>Menu:</span>
+                                        <span id = "menuInput" style={{fontSize: '1.4rem', marginLeft: '10px'}}>
                                             <input name="menu" placeholder="Enter the Menu" value={this.state.menu} type="text" onChange={this.handleInputChange}/>
                                         </span>
-                                        <span class = "truckName" style = {{marginLeft: '80px', display: 'inline-block', fontSize: '1.5rem', textAlign: 'left', fontWeight: 'bold', marginTop: '5px'}}>Truck Name:</span>
-                                        <span id = "truckName" style={{fontSize: '1.5rem', textAlign: 'left', marginLeft: '10px'}}>
+                                    </div>
+
+                                    <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
+                                        <span class = "truckName" style = {{fontSize: '1.4rem', fontWeight: 'bold', marginTop: '5px'}}>Truck Name:</span>
+                                        <span id = "truckName" style={{fontSize: '1.4rem', marginLeft: '10px'}}>
                                             <input name="truckName" placeholder="Enter the Truck Name" value={this.state.truckName} type="text" onChange={this.handleInputChange}/>
                                         </span>
-                                        <span class = "description" style = {{marginLeft: '80px', display: 'inline-block', fontSize: '1.5rem', textAlign: 'left', fontWeight: 'bold', marginTop: '5px'}}>Description:</span>
-                                        <span id = "descriptionInput" style={{fontSize: '1.5rem', textAlign: 'left', marginLeft: '10px'}}>
+                                    </div>
+
+                                    <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
+                                        <span class = "description" style = {{fontSize: '1.4rem', fontWeight: 'bold', marginTop: '5px'}}>Description:</span>
+                                        <span id = "descriptionInput" style={{fontSize: '1.4rem', marginLeft: '10px'}}>
                                             <input name="description" placeholder="Enter the Description" value={this.state.description} type="text" onChange={this.handleInputChange}/>
                                         </span>
-                                        <span class = "details" style = {{marginLeft: '80px', display: 'inline-block', fontSize: '1.5rem', textAlign: 'left', fontWeight: 'bold', marginTop: '5px'}}>Details:</span>
-                                        <span id = "detailsInput" style={{fontSize: '1.5rem', textAlign: 'left', marginLeft: '10px'}}>
+                                    </div>
+
+                                    <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
+                                        <span class = "details" style = {{fontSize: '1.4rem', fontWeight: 'bold', marginTop: '5px'}}>Details:</span>
+                                        <span id = "detailsInput" style={{fontSize: '1.4rem', marginLeft: '10px'}}>
                                             <input name="details" placeholder="Enter the Details" value={this.state.details} type="text" onChange={this.handleInputChange}/>
                                         </span>
-                                        <span class = "route" style = {{marginLeft: '80px', display: 'inline-block', fontSize: '1.5rem', textAlign: 'left', fontWeight: 'bold', marginTop: '5px'}}>Route:</span>
-                                        <span id = "routeInput" style={{fontSize: '1.5rem', textAlign: 'left', marginLeft: '10px'}}>
+                                    </div>
+
+                                    <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
+                                        <span class = "route" style = {{fontSize: '1.4rem', fontWeight: 'bold', marginTop: '5px'}}>Route:</span>
+                                        <span id = "routeInput" style={{fontSize: '1.4rem', marginLeft: '10px'}}>
                                             <input name="route" placeholder="Enter the Route" value={this.state.route} type="text" onChange={this.handleInputChange}/>
                                         </span>
-                                        <span class = "schedule" style = {{marginLeft: '80px', display: 'inline-block', fontSize: '1.5rem', textAlign: 'left', fontWeight: 'bold', marginTop: '5px'}}>Schedule:</span>
-                                        <span id = "scheduleInput" style={{fontSize: '1.5rem', textAlign: 'left', marginLeft: '10px'}}>
+                                    </div>
+
+                                    <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
+                                        <span class = "schedule" style = {{fontSize: '1.4rem', fontWeight: 'bold', marginTop: '5px'}}>Schedule:</span>
+                                        <span id = "scheduleInput" style={{fontSize: '1.4rem', marginLeft: '10px'}}>
                                             <input name="schedule" placeholder="Enter the Schedule" value={this.state.schedule} type="text" onChange={this.handleInputChange}/>
                                         </span>
+                                    </div>
+
                                     <br></br>
-                                    <span style={{marginLeft: '200px'}}>
+
+                                    <span style={{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
                                         <input type="submit" value="Create"/>
                                     </span>
                                 </form>
