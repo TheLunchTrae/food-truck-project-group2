f82e6e35f235fb99bbe47abfae0a85ccf437a2de
Tyler Ross <Tyler_Ross3@Baylor.edu>
2021-11-11 21:49:04 -0600

    List in search

--- food-truck-frontend/src/pages/search.js
+++ food-truck-frontend/src/pages/search.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import ReactList from 'react-list';
 
 class Search extends Component {
     constructor(props) {
-        super();
+        super(props);
+        this.state = { foodTrucks: [] };
+        this.componentWillMount = this.componentWillMount.bind(this);
+        this.itemRenderer = this.itemRenderer.bind(this);
     }
     handleChangeStatus(event) {
     }
@@ -10,7 +14,13 @@ class Search extends Component {
     }
     handleSubmit(event) {
     }
+    componentWillMount() {
+        this.state.foodTrucks = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+    }
     componentDidMount() {
+    }
+    itemRenderer(index, key) {
+        return <div key={key}>{this.state.foodTrucks[index].name}</div>;
     }
     render() {
         return (
@@ -21,6 +31,10 @@ class Search extends Component {
                             <div class = "block" style = {{alignContent: 'center', borderRadius: '100px', background: '#F9D5A7', width: '50%', padding: '20px', margin: '35px auto', textAllign: 'center', border: '3px solid black'}}>
 
                                 <span class="heading" style={{color: "#0F52BA", display: 'block', fontSize: '2.5rem', textAlign: 'center', fontWeight: 'bold'}}>Search</span>
+
+                                <div style={{maxHeight: 100, overflow: 'auto'}}>
+                                    <ReactList length={this.state.foodTrucks.length} type='uniform' />
+                                </div>
                             </div>
                         </div>
                     </div>
