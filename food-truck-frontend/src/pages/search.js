b9e84c8d76c3156d0c08ff82cf928f107e02c9fd
Tyler Ross <Tyler_Ross3@Baylor.edu>
2021-11-17 17:18:15 -0600

    Redirect to truck details page

--- food-truck-frontend/src/pages/search.js
+++ food-truck-frontend/src/pages/search.js
@@ -6,12 +6,13 @@ import MenuBar from '../menuBar';
 class Search extends Component {
     constructor(props) {
         super(props);
-        this.state = { foodTrucksRec: [], foodTrucksSearch: [], search: '' };
+        this.state = { foodTrucksRec: [], foodTrucksSearch: [], search: '', listColor: 'white' };
         this.componentDidMount = this.componentDidMount.bind(this);
         this.renderRecommended = this.renderRecommended.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
         this.renderSearch = this.renderSearch.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleTruckClick = this.handleTruckClick.bind(this);
     }
     handleChangeStatus(event) {
     }
@@ -32,7 +33,7 @@ class Search extends Component {
                 }
                 }).then(res => {
                     console.log(res.data);
-                    const foodTrucks = res.data.map(obj => ({truckName: obj.truckName}));
+                    const foodTrucks = res.data.map(obj => ({truckName: obj.truckName, truckId: obj.truckId}));
                     this.setState({ foodTrucksSearch: foodTrucks });
             });
         }
@@ -45,7 +46,7 @@ class Search extends Component {
                 }
                 }).then(res => {
                     console.log(res.data);
-                    const foodTrucks = res.data.map(obj => ({truckName: obj.truckName}));
+                    const foodTrucks = res.data.map(obj => ({truckName: obj.truckName, truckId: obj.truckId}));
                     this.setState({ foodTrucksSearch: foodTrucks });
             });
         }
@@ -57,15 +58,19 @@ class Search extends Component {
             }
         }).then(res => {
             console.log(res.data);
-            const foodTrucks = res.data.map(obj => ({truckName: obj.truckName}));
+            const foodTrucks = res.data.map(obj => ({truckName: obj.truckName, truckId: obj.truckId}));
             this.setState({ foodTrucksRec: foodTrucks });
         });
     }
     renderRecommended(index, key) {
-        return <div key={key}>{this.state.foodTrucksRec[index].truckName}</div>;
+        return <div key={key} onClick={this.handleTruckClick} id={this.state.foodTrucksRec[index].truckId}>{this.state.foodTrucksRec[index].truckName}</div>;
     }
     renderSearch(index, key) {
-        return <div key={key}>{this.state.foodTrucksSearch[index].truckName}</div>;
+        return <div key={key} onClick={this.handleTruckClick} id={this.state.foodTrucksSearch[index].truckId}>{this.state.foodTrucksSearch[index].truckName}</div>;
+    }
+    handleTruckClick(event) {
+        console.log(event.target.id);
+        window.location.href="/truckDetails?truckId=" + event.target.id;
     }
     render() {
         return (
@@ -79,7 +84,7 @@ class Search extends Component {
                                 <span class="heading" style={{color: "#0F52BA", display: 'block', fontSize: '2.5rem', textAlign: 'center', fontWeight: 'bold'}}>Search</span>
 
                                 <div style={{marginLeft: '100px', maxHeight: 100, overflow: 'auto', width: '70%', justifyContent: 'left', display: 'flex'}}>
-                                    <ReactList itemRenderer = {this.renderRecommended} length={this.state.foodTrucksRec.length} type='uniform' />
+                                    <ReactList itemRenderer = {this.renderRecommended} length={this.state.foodTrucksRec.length} type='uniform'  />
 
 
                                     <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
