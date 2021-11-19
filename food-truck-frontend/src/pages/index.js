8c9dbac8c846ca94a7cab7d4abd24bbd81200cab
JoshuaHunter16 <jchunter816@gmail.com>
2021-11-18 21:31:15 -0600

    Fixing homepage to not be ugly

--- food-truck-frontend/src/pages/index.js
+++ food-truck-frontend/src/pages/index.js
@@ -8,10 +8,10 @@ function HomePage() {
             <body class={styles.body}>
                 <MenuBar/>
                 <div class="banner-text">
-                    <div class="text-area" style = {{textAlign: 'center', width: '75%', margin: '0 auto'}}>
-                        <h2 style = {{fontSize: '75px', color: '#FFFFFF', margin: '10% 0 0'}}>Food Truck Finder</h2>
-                        <h3 style = {{color: '#FFFFFF', textTransform: 'uppercase', margin: '0 0 15px', fontSize: '35px'}}>Group 2</h3>
-                        <p style = {{fontSize: '18px', color: '#FFFFFF', width: '70%', margin: '0 auto', lineHeight: '1.9'}}>Welcome to the Food Truck Finder Application for Group 2!</p>
+                    <div class="text-area" style = {{textAlign: 'center', width: '75%', margin: '0 auto', fontFamily: 'Playfair Display'}}>
+                        <h2 style = {{fontSize: '75px', color: '#000000', margin: '10% 0 0'}}>Food Truck Finder</h2>
+                        <h3 style = {{color: '#000000', textTransform: 'uppercase', margin: '0 0 15px', fontSize: '35px'}}>Group 2</h3>
+                        <p style = {{fontSize: '18px', color: '#000000', width: '70%', margin: '0 auto', lineHeight: '1.9'}}>Welcome to the Food Truck Finder Application for Group 2!</p>
                     </div>
                 </div>
             </body>
