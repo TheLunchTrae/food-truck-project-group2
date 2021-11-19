344ecbbd2b1565b054cb9853a65521262420d103
TheLunchTrae <56175379+TheLunchTrae@users.noreply.github.com>
2021-11-19 00:13:04 -0600

    Signup now keeps inputs open once a value has been input

--- food-truck-frontend/src/pages/signup.js
+++ food-truck-frontend/src/pages/signup.js
@@ -60,34 +60,34 @@ class Signup extends Component {
                                 <div class={styles.wrapper}>
 
                                     <div class={styles.formnput}>
-                                        <input id="username" class={styles.formelementinput} value={this.state.username} type="input" placeholder="Please create a username"  required onChange={this.handleInputChange}/>
+                                        <input id="username" name="username" class={styles.formelementinput} value={this.state.username} type="input" placeholder="Please create a username"  required onChange={this.handleInputChange}/>
                                         <div class={styles.formelementbar}></div>
                                         <label class={styles.formelementlabel} for="username">Username</label>
                                     </div>
 
                                     <div class={styles.formnput}>
-                                        <input id="emailInput" class={styles.formelementinput} type="input" pattern=".*@.*\..*" title="Must be in valid email format" placeholder="Please fill in your email"  value={this.state.email} required onChange={this.handleInputChange}/>
+                                        <input id="emailInput" name="email" class={styles.formelementinput} type="input" pattern=".*@.*\..*" title="Must be in valid email format" placeholder="Please fill in your email"  value={this.state.email} required onChange={this.handleInputChange}/>
                                         <div class={styles.formelementbar}></div>
                                         <label class={styles.formelementlabel} for="email">Email</label>
                                     </div>
 
                                     <div class={styles.formnput}>
-                                        <input id="passwordInput" class={styles.formelementinput} type="password" name="password" placeholder="Please enter a password" required value={this.state.password} onChange={this.handleInputChange}/>
+                                        <input id="passwordInput" name="password" class={styles.formelementinput} type="password" placeholder="Please enter a password" required value={this.state.password} onChange={this.handleInputChange}/>
                                         <div class={styles.formelementbar}></div>
                                         <label class={styles.formelementlabel} for="password">Password</label>
                                     </div>
 
                                     <div class={styles.formnput}>
-                                        <select class={styles.formelementinput} name="usertype" id="userType">
+                                        <select id="userTypeInput" name="usertype" class={styles.formelementinput}>
                                             <option value="Customer">Customer</option>
                                             <option value="Owner">Food Truck Owner</option>
                                         </select>
                                         <div class={styles.formelementbar}></div>
-                                        <label class={styles.formelementlabel} for="password">User Type</label>
+                                        <label class={styles.formelementlabel} for="usertype">User Type</label>
                                     </div>
 
                                     <div style = {{display: 'block', alignContent: 'center', margin: '0 auto', textAlign: 'center', padding: '5px 0'}}>
-                                        <button type="submit" style = {{background: '#a9a9a9', fontSize: '17px', cursor: 'pointer'}}>Subscribe</button>
+                                        <button type="submit" style = {{background: '#a9a9a9', fontSize: '17px', cursor: 'pointer'}}>Sign Up!</button>
                                     </div>
                                 </div>
                             </form>
