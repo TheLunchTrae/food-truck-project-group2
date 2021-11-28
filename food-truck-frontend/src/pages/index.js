32a6688600aeac7dc0135f3c004762588a790bea
TheLunchTrae <56175379+TheLunchTrae@users.noreply.github.com>
2021-11-28 15:59:16 -0600

    Changes to .gitignore for api

--- food-truck-frontend/src/pages/index.js
+++ food-truck-frontend/src/pages/index.js
@@ -17,7 +17,7 @@ function HomePage() {
                         <h3 style = {{color: '#000000', textTransform: 'uppercase', margin: '0 0 15px', fontSize: '35px'}}>Group 2</h3>
                         <p style = {{fontSize: '18px', color: '#000000', width: '70%', margin: '0 auto', lineHeight: '1.9'}}>Welcome to the Food Truck Finder Application for Group 2!</p>
                     </div>
-
+                    
                     <div style={{marginTop: '30px', textAlign: 'center', display: 'block'}}>
                         <img style={{width: '400px', height: '240px'}} src={"https://www.bakemag.com/ext/resources/images/TiffsTreats_FoodTruck.jpg?t=1604679543"}/>
                     </div>
