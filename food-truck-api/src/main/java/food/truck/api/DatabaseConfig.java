0c4cbcdcfc8c7efdacf82ec60c9122b1e048d8d6
TheLunchTrae <56175379+TheLunchTrae@users.noreply.github.com>
2021-11-28 20:53:59 -0600

    Made database configuration environment adaptable

---
+++ food-truck-api/src/main/java/food/truck/api/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package food.truck.api;
+
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+        return new HikariDataSource(config);
+    }
+}
