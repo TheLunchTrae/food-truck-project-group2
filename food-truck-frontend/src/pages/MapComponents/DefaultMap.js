3cc3dc06752abb452536b26dfa91fd784f688b9f
TheLunchTrae <56175379+TheLunchTrae@users.noreply.github.com>
2021-12-06 20:20:25 -0600

    Change containerStyle prop

--- food-truck-frontend/src/pages/MapComponents/DefaultMap.js
+++ food-truck-frontend/src/pages/MapComponents/DefaultMap.js
@@ -4,13 +4,17 @@ import Geocode from "react-geocode";
 
 const APICode = "AIzaSyAFiDEFB5H7qlYn-LeipCsfkCYt-nm4AGk"
 
+const containerStyle = {
+  width: '400px', 
+  height: '400px'
+}
+
 Geocode.setApiKey(APICode);
 Geocode.setLanguage("en");
 
 class DefaultMap extends Component {
 
   static defaultProps = {
-    containerStyle: { width: '400px', height: '400px'},
     center: { lat: 31.547164416064646, lng: -97.11819049760257 },
     markers: []
   }
@@ -29,8 +33,7 @@ class DefaultMap extends Component {
   render(props) {
     return (
       <LoadScript googleMapsApiKey={APICode} libraries={["places"]}>
-        <GoogleMap mapContainerStyle={props.containerStyle} center={props.center} zoom={10}>
-        </GoogleMap>
+        <GoogleMap mapContainerStyle={containerStyle} center={props.center} zoom={10}></GoogleMap>
       </LoadScript>
     )
   }
